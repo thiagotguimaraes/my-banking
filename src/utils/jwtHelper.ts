4436b5680adbaf6f6314cf16efe9e3f47f6a2c93
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-03 11:50:43 +0400

    minor fix to read properly from .env file variables

--- src/utils/jwtHelper.ts
+++ src/utils/jwtHelper.ts
@@ -1,11 +1,10 @@
 import jwt from 'jsonwebtoken'
 
-const SECRET_KEY =
-	process.env.JWT_SECRET ||
-	'uhfeuwhjf083hfurenhuwhjf8054hw0f8hockwpoickjq0r8jf89fhw3hfuhrnewgh478ghherug9gf9eouencijwaec'
+const SECRET_KEY: jwt.Secret = process.env.JWT_SECRET || 'fuwehfhwepofhoewurfhuoewrf'
+const EXPIRATION_KEY: jwt.SignOptions['expiresIn'] = Number(process.env.JWT_SECRET_EXPIRES_IN || 60000)
 
 export const generateToken = (userId: string, role: string): string => {
-	return jwt.sign({ userId, role }, SECRET_KEY, { expiresIn: '1h' })
+	return jwt.sign({ userId, role }, SECRET_KEY, { expiresIn: EXPIRATION_KEY })
 }
 
 export const verifyToken = (token: string) => {
