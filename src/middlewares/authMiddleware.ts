b8a747be719808e21cbea1ccf5d9b09964ba8b90
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-03 11:24:00 +0400

    fixing small types errors

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -5,16 +5,18 @@ export interface AuthRequest extends Request {
 	user?: { userId: string; role: string }
 }
 
-export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
+export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction): void => {
 	const token = req.header('Authorization')?.split(' ')[1]
 
 	if (!token) {
-		return res.status(401).json({ message: 'Unauthorized: No token provided' })
+		res.status(401).json({ message: 'Unauthorized: No token provided' })
+		return
 	}
 
 	const decoded = verifyToken(token)
 	if (!decoded) {
-		return res.status(401).json({ message: 'Unauthorized: Invalid token' })
+		res.status(401).json({ message: 'Unauthorized: Invalid token' })
+		return
 	}
 
 	req.user = decoded as { userId: string; role: string }
--- src/middlewares/transactionsMiddleware.ts
+++ src/middlewares/transactionsMiddleware.ts
@@ -1,15 +1,16 @@
 import { Request, Response, NextFunction } from 'express'
+import { AuthRequest } from './authMiddleware'
 
-export interface TransactionRequest extends Request {
+export interface TransactionRequest extends AuthRequest {
 	body: { amount: number }
-	user: { userId: number }
 }
 
-export const transactionsMiddleware = (req: TransactionRequest, res: Response, next: NextFunction) => {
+export const transactionsMiddleware = (req: TransactionRequest, res: Response, next: NextFunction): void => {
 	const { amount } = req.body
 
 	if (!amount || amount <= 0) {
-		return res.status(400).json({ message: 'Invalid amount' })
+		res.status(400).json({ message: 'Invalid amount' })
+		return
 	}
 
 	next()
